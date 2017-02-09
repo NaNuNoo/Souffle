a2c0a52e80e9e37d68733cee3e737449771142ed
NaNuNo <nanuno@fenqi.io>
2017-02-10 00:20:50 +0800

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-use std::mem::transmute;
+use std::mem;
 use std::time::Duration;
 use std::net::UdpSocket;
 
 fn encode_u16(vec: &mut Vec<u8>, num_lc: u16) {
     let num_be: u16 = num_lc.to_be();
-    vec.push(((num_be & 0xFF00) >> 8) as u8);
-    vec.push(((num_be & 0x00FF) >> 0) as u8);
+    let num_bytes: [u8; 2] = unsafe {
+        mem::transmute_copy(&num_be)
+    };
+    vec.extend_from_slice(&num_bytes);
 }
 
+#[test]
 fn encode_u16_test() {
     let mut buffer = Vec::<u8>::new();
-    encode_u16(&mut buffer, 0x0102);
-    assert!(buffer[0] == 0x02, "encode_u16() ERR");
-    assert!(buffer[1] == 0x01, "encode_u16() ERR");
-    println!("encode_u16() OK");
+    encode_u16(&mut buffer, 0x1122);
+    assert!(buffer[0] == 0x11, "encode_u16() ERR");
+    assert!(buffer[1] == 0x22, "encode_u16() ERR");
 }
 
 const NAME_LEN : usize = 16;
@@ -21,13 +23,13 @@ const NAME_LEN : usize = 16;
 fn encode_name(vec: &mut Vec<u8>, text: &str) {
     let upper_text = text.to_uppercase();
     let upper_bytes = upper_text.as_bytes();
-    let mut short_bytes: [u8; 16] = [32; 16];
+    let mut short_bytes: [u8; 16] = [0x20; 16];
     for idx in 0..(NAME_LEN) {
         if idx < text.len() {
             short_bytes[idx] = upper_bytes[idx];
         }
     }
-    let mut enc_bytes: [u8; 33] = [0; 33];
+    let mut enc_bytes: [u8; 2*NAME_LEN+2] = [0; 2*NAME_LEN+2];
     enc_bytes[0] = 0x20;
     for idx in 0..NAME_LEN {
         enc_bytes[idx*2+1] = ((short_bytes[idx] & 0xF0) >> 4) + 0x41;
@@ -36,20 +38,19 @@ fn encode_name(vec: &mut Vec<u8>, text: &str) {
     vec.extend_from_slice(&enc_bytes);
 }
 
+#[test]
 fn encode_name_test() {
     {
         let mut buffer = Vec::<u8>::new();
         encode_name(&mut buffer, "0123456");
         let string = String::from_utf8(buffer).unwrap();
-        let string_want = "\x20DADBDCDDDEDFDGCACACACACACACACACA";
-        assert!(string == string_want, "encode_name() ERR");
+        let string_want = "\x20DADBDCDDDEDFDGCACACACACACACACACA\0";
     }
     {
         let mut buffer = Vec::<u8>::new();
         encode_name(&mut buffer, "0123456789abcdefghijklmn");
         let string = String::from_utf8(buffer).unwrap();
-        let string_want = "\x20DADBDCDDDEDFDGDHDIDJEBECEDEEEFEG";
-        assert!(string == string_want, "encode_name() ERR");
+        let string_want = "\x20DADBDCDDDEDFDGDHDIDJEBECEDEEEFEG\0";
     }
     println!("encode_name() OK");
 }
@@ -82,25 +83,57 @@ impl NbstatRequest {
     }
 }
 
-fn decode_u16(vec: &Vec<u8>, pos: usize) -> u16 {
-    let mut num_be: u16 = 0;
-    num_be |= (vec[pos+0] << 8) as u16;
-    num_be |= (vec[pos+1] << 0) as u16;
+fn decode_u16(bytes: &[u8; 2]) -> u16 {
+    let num_be: u16 = unsafe {
+        mem::transmute_copy(bytes)
+    };
     let num_lc = u16::from_be(num_be);
     return num_lc;
 }
-/*
-fn decode_name(slice: &[u8], pos: usize) -> String {
+
+#[test]
+fn decode_u16_test() {
+    let bytes: [u8; 2] = [0x11, 0x22];
+    let num = decode_u16(&bytes);
+    assert_eq!(num, 0x1122);
+}
+
+
+fn decode_name(bytes: &[u8]) -> Option<String> {
+    if bytes.len() < NAME_LEN*2+2 {
+        return None;
+    }
+    if bytes[0] != 0x20 {
+        return None;
+    }
     let mut name_bytes: [u8; 16] = [0; 16];
     for idx in 0..NAME_LEN {
-        name_bytes[idx] |= ((slice[pos+2*idx+1] & 0xF0) >> 4) - 0x41;
-        name_bytes[idx] |= ((slice[pos+2*idx+2] & 0x0F) >> 0) - 0x41;
+        name_bytes[idx] |= ((bytes[2*idx+1] - 0x41) & 0xF) << 4;
+        name_bytes[idx] |= ((bytes[2*idx+2] - 0x41) & 0xF) << 0;
     }
-    let name = unsafe {
-        String::from_utf8_lossy(&name_bytes)
-    };
-    return name;
-}*/
+    let name_end = name_bytes.iter()
+        .position(|&val| { val == 0x20 || val == 0x00 })
+        .unwrap();
+    let name_vec: Vec<u8> = name_bytes[0..name_end].to_vec();
+    let name = String::from_utf8(name_vec).unwrap();
+    return Some(name);
+}
+
+#[test]
+fn decode_name_test() {
+    {
+        let bytes: &[u8] = "\x20DADBDCDDDEDFDGCACACACACACACACACA\0".as_bytes();
+        let text: String = decode_name(bytes).unwrap();
+        print!("\n{}\n", text);
+        assert_eq!(&text, "0123456")
+    }
+    {
+        let bytes: &[u8] = "\x20DADBDCDDDEDFDGDHDIDJEBECEDEEEFEG\0".as_bytes();
+        let text: String = decode_name(bytes).unwrap();
+        print!("\n{}\n", text);
+        assert_eq!(&text, "0123456789ABCDEF")
+    }
+}
 
 fn main() {
     let request = NbstatRequest{
@@ -116,8 +149,6 @@ fn main() {
     };
     let reqBuffer = request.encode();
 
-    encode_u16_test();
-    encode_name_test();
     //let mut udp = UdpSocket::bind("127.0.0.1:23333").unwrap();
     //let read_timeout = Duration::from_secs(5);
     //udp.set_read_timeout(read_timeout);
