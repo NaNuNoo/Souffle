3552085e285e74d43d6ec66c2f0022a16b302ac6
NaNuNo <nanuno@fenqi.io>
2017-02-14 23:03:52 +0800

    negotiate protocol

--- src/samba.rs
+++ src/samba.rs
@@ -22,18 +22,18 @@ impl Context {
             sequence: -1,
             host: String::from(host),
             name: String::from(name),
+            mac_key: String::from(name)
         };
     }
 }
 
-const HEADER_SIZE: usize = 32;
-const NEGOTIATE_PROTOCOL_SIZE: usize = 14;
+const VEC_INIT_SIZE = 64;
 
 const SMB_COM_NEGOTIATE: u8 = 0x72;
 
 #[allow(dead_code)]
 fn encode_header(context: &Context, command: u8) -> Vec<u8> {
-    let mut buffer: Vec<u8> = Vec::with_capacity(HEADER_SIZE);
+    let mut buffer: Vec<u8> = Vec::with_capacity(VEC_INIT_SIZE);
     buffer.extend_from_slice(&[
         0xFF, ('S' as u8), ('M' as u8), ('B' as u8)
     ]);
@@ -51,17 +51,32 @@ fn encode_header(context: &Context, command: u8) -> Vec<u8> {
     return buffer;
 }
 
-#[derive(Default)]
-struct NegotiateProtocol {
+#[allow(dead_code)]
+fn encode_packet(context: &Context, header &[u8], parameter &[u8], data &[u8]) -> Vec<u8> {
+    let packet: Vec<u8> = Vec::with_capacity(VEC_INIT_SIZE);
+    packet.extend_from_slice(header);
+    packet.extend_from_slice(&encode_u8_le(parameter.len() / 2));
+    packet.extend_from_slice(parameter);
+    packet.extend_from_slice(&encode_u16_le(data.len()));
+    packet.extend_from_slice(data);
+    return packet;
 }
 
 #[allow(dead_code)]
 fn encode_negotiate_protocol(args: &NegotiateProtocol) -> Vec<u8> {
-    let mut buffer: Vec<u8> = Vec::with_capacity(NEGOTIATE_PROTOCOL_SIZE);
+    let context: Context = Context::new("192.168.1.8:445", "");
+    // header
+    let header: Vec<u8> = encode_header(&context, SMB_COM_NEGOTIATE);
+    // parameter
+    let mut parameter: Vec<u8> = vec![0; 1];
+    // data
+    let mut data: Vec<u8> = Vec::with_capacity(VEC_INIT_SIZE);
     buffer.extend_from_slice(&encode_u8_le(2));
     buffer.extend_from_slice("NT LM 0.12\0".as_bytes());
     buffer.extend_from_slice(&encode_u8_le(2));
     buffer.extend_from_slice("\0".as_bytes());
+    // packet
+    let packet = encode_packet(&context, &header, &parameter, &data);
     return buffer;
 }
 
