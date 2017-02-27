82c45264fdc201f19ab3023509014af742c80f72
NaNuNo <nanuno@fenqi.io>
2017-02-27 23:14:30 +0800

    build script

--- build.rs
+++ build.rs
@@ -1,64 +1,87 @@
+use std::error;
 use std::process;
 use std::fs;
 use std::path;
 
-fn list_cpp(mod_name: str) -> Vec<String> {
-      let mod_path = path::Path("./src/" + mod_name);
-      if !mod_path.is_dir() {
-            panic!("Not a mod {:?}", mod_name);
-      }
-      let cpp_base_vec: Vec<String> = Vec::new();
-      let cpp_iter = fs::read_dir(mod_path).unwrap();
-      for iter in cpp_iter {
-            let cpp_path = itr.path();
-            if !cpp_path.is_file() {
-                  continue;
-            }
-            if cpp_path.extension() == "cpp" {
-                  let os_cpp_base = cpp_vec.file_stem().unwrap();
-                  let cpp_base = String::from(os_cpp_base.to_str().unwrap());
-                  cpp_base_vec.push(cpp_base);
-            }
-      }
-      return cpp_base_vec;
-}
+type BoxErr = Box<error::Error>;
 
-fn compile_cpp(mod_name: str, cpp_base_vec: &[String]) {
-      for cpp_base in &cpp_base_vec {
-            let cpp_input = format!(".\\src\\{}\\{}.cpp", mod_name, cpp_base);
-            let cpp_output = format!("-Fo:\".\\target\\native\\{}\\{}.obj\"", mod_name, cpp_base);
-            process::Command::new("cl.exe")
-                  .args(&["/c", "\\Ox", cpp_input, cpp_output])
-                  .status()
-                  .expect(
-                        format!("Compile c++ lib ERR. [{}]", mod_name)
-                  );
-      }
-}
-
-fn link_cpp(mod_name: str, cpp_base_vec: &[String]) {
-      let obj_input_vec: Vec<String> =
-            cpp_base.iter()
-            .map(|cpp_path|{
-                  format!(".\\target\\native\\{}\\{}.obj", mod_name, cpp_base)
-            })
-            .collect();
-      let lib_output = format!("/OUT:\".\\target\\native\\{}.lib\"", mod_name, cpp_base);
-      process::Command::new("lib.exe")
-            .args(&obj_input_vec)
-            .arg(lib_output)
-            .status()
-            .expect(
-                  format!("Compile c++ lib ERR. [{}]", mod_name)
+fn list_cpp(mod_name: &str) -> Result<Vec<String>, BoxErr> {
+    let mod_path = path::Path::new(
+        &format!(".\\src\\{}", mod_name)
+    );
+    if !mod_path.is_dir() {
+        return Err(
+            format!("Not a mod {}", mod_name)
+        );
+    }
+    let file_name_vec: Vec<String> = Vec::new();
+    for iter in try!(fs::read_dir(mod_path)) {
+        let file_path = try!(iter).path();
+        if !file_path.is_file() {
+            continue;
+        }
+        let ext_name = file_path.extension().unwrap_or("");
+        if ext_name == ".cpp" {
+            let file_name_os = try!(
+                file_path.file_stem().ok_or(Err(file_path))
             );
+            let file_name = String::from(
+                try!(file_name_os.to_str())
+            );
+            file_name_vec.push(file_name);
+        }
+    }
+    return Ok(file_name_vec);
 }
 
-fn build_cpp(mod_name: str) {
-      let cpp_base_vec = list_cpp(mod_name);
-      compile_cpp(mod_name, cpp_base_vec);
-      link_cpp(mod_name, cpp_base_vec);
+fn compile_cpp(mod_name: &str, file_name_vec: &[String]) -> Result<(), BoxErr> {
+    let cpp_name_vec: Vec<String> =
+        file_name_vec.iter()
+        .map(|file_name|{
+            format!("..\\..\\src\\{}\\{}.cpp", mod_name, file_name)
+        })
+        .collect();
+    try!(
+        process::Command::new("cl.exe")
+        .current_dir(
+            format!(".\\target\\native\\{}", mod_name)
+        )
+        .args(&["/c", "\\Ox"])
+        .args(&cpp_name_vec)
+        .status()
+    );
+    return Ok(());
+}
+
+fn link_cpp(mod_name: &str, file_name_vec: &[String]) -> Result<(), BoxErr> {
+    let obj_name_vec: Vec<String> =
+        file_name_vec.iter()
+        .map(|file_name|{
+            format!("{}\\{}.obj", mod_name, file_name)
+        })
+        .collect();
+    try!(
+        process::Command::new("lib.exe")
+        .current_dir(".\\target\\native")
+        .args(&obj_name_vec)
+        .arg(
+            format!("/OUT:\"{}.lib\"", mod_name)
+        )
+        .status()
+    );
+    return Ok(());
+}
+
+fn build_cpp(mod_name: &str) -> Result<(), BoxErr> {
+    let file_base_vec = try!(list_cpp(mod_name));
+    try!(compile_cpp(mod_name, &file_base_vec));
+    try!(link_cpp(mod_name, &file_base_vec));
+    return Ok(());
 }
 
 fn main() {
-      println!("cargo:rustc-link-search=./target/native/");
+    fs::create_dir(".\\target");
+    fs::create_dir(".\\target\\native");
+    build_cpp("");
+    //println!("cargo:rustc-link-search=./target/native/");
 }
